9703243b5aa668d520825098759775ff14b636b4
Revathi Mahalingam <103559154+revathimsc@users.noreply.github.com>
2023-05-22 16:34:14 +0530

    Add files via upload

---
+++ src/components/AddBook.js
@@ -0,0 +1,131 @@
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormLabel,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AddBook = () => {
+  const history = useNavigate();
+  const [inputs, setInputs] = useState({
+    name: "",
+    description: "",
+    price: "",
+    author: "",
+
+    image: "",
+  });
+  const [checked, setChecked] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+    // console.log(e.target.name, "Value", e.target.value);
+  };
+
+  const sendRequest = async () => {
+    await axios
+      .post("http://localhost:5000/books", {
+        name: String(inputs.name),
+        author: String(inputs.author),
+        description: String(inputs.description),
+        price: Number(inputs.price),
+        image: String(inputs.image),
+        available: Boolean(checked),
+      })
+      .then((res) => res.data);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs, checked);
+    sendRequest().then(() => history("/books"));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent={"center"}
+        maxWidth={600}
+        alignContent={"center"}
+        alignSelf="center"
+        marginLeft={"auto"}
+        marginRight="auto"
+        marginTop={5}
+      >
+        <FormLabel>Name</FormLabel>
+        <TextField
+          value={inputs.name}
+          onChange={handleChange}
+          placeholder="Enter The Name Of The Book"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="name"
+        />
+        <FormLabel>Author</FormLabel>
+        <TextField
+          value={inputs.author}
+          onChange={handleChange}
+          placeholder="Enter The Name Of The Author"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="author"
+        />
+        <FormLabel>Description</FormLabel>
+        <TextField
+          value={inputs.description}
+          onChange={handleChange}
+          placeholder="Enter The Name Of The Description"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="description"
+        />
+        <FormLabel>Price</FormLabel>
+        <TextField
+          value={inputs.price}
+          onChange={handleChange}
+          placeholder="Enter The Name Of The Price"
+          type="number"
+          margin="small"
+          fullWidth
+          variant="outlined"
+          name="price"
+        />
+        <FormLabel>Image</FormLabel>
+        <TextField
+          value={inputs.image}
+          onChange={handleChange}
+          placeholder="Enter The Image Of The URL"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="image"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={checked} onChange={() => setChecked(!checked)} />
+          }
+          label="Available"
+        />
+
+        <Button variant="contained" type="submit">
+          SUBMIT
+        </Button>
+      </Box>
+    </form>
+  );
+};
+
+export default AddBook;
---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import { Button, Typography, Box } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <Box display="flex" flexDirection="column" alignItems="center">
+     
+        <Button
+          LinkComponent={Link}
+          to="/books"
+          sx={{ marginTop: 10, background: "purple" }}
+          variant="contained"
+          
+        >
+         <Typography variant="h5">View Books</Typography>
+         
+         
+
+        </Button>
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
