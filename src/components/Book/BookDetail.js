945f7d7586a1fc1062e0be37e179892c20e5cac1
Revathi Mahalingam <103559154+revathimsc@users.noreply.github.com>
2023-05-22 16:56:52 +0530

    Update BookDetail.js

--- src/components/Book/BookDetail.js
+++ src/components/Book/BookDetail.js
@@ -18,7 +18,7 @@ const BookDetail = () => {
   useEffect(() => {
     const fetchHandler = async () => {
       await axios
-        .get(`https://bookstore-backend-83w0.onrender.com//books/${id}`)
+        .get(`https://bookstore-backend-83w0.onrender.com/books/${id}`)
         .then((res) => res.data)
         .then((data) => setInputs(data.book));
     };
@@ -27,7 +27,7 @@ const BookDetail = () => {
 
   const sendRequest = async () => {
     await axios
-      .put(`https://bookstore-backend-83w0.onrender.com//books/${id}`, {
+      .put(`https://bookstore-backend-83w0.onrender.com/books/${id}`, {
         name: String(inputs.name),
         author: String(inputs.author),
         description: String(inputs.description),
